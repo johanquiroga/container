39e123c3c86958842140e9b43e6eed59d9195b88
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-15 09:34:51 -0500

    First commit

---
+++ src/ContainerException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace JohanQuiroga\Container;
+
+use Exception;
+
+class ContainerException extends Exception
+{
+}
---
+++ src/Provider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace JohanQuiroga\Container;
+
+
+abstract class Provider
+{
+    /**
+     * @var \JohanQuiroga\Container\Container
+     */
+    protected $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    abstract public function register();
+}
